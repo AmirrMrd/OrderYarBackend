c3f3138bce11c5e1620a866b804f1477da05d50f
Amir <amirrmrd97@gmail.com>
2024-04-21 00:24:15 +0330

    Add project files.

---
+++ BarberShopProject/Infrastructure/BarberShopContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BarberShopProject.Infrastructure
+{
+    public class BarberShopContext : DbContext
+    {
+        public BarberShopContext(DbContextOptions<BarberShopContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Blog> Blogs { get; set; }
+
+        #region Required
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>()
+                .Property(b => b.Url)
+                .IsRequired();
+        }
+        #endregion
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string? Url { get; set; }
+    }
+}
